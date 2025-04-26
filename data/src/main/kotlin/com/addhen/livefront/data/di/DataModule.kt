bd6f64fbba3842de617422be4dfa17dda938a73f
Henry Addo <henry@addhen.org>
2025-04-26 16:17:12 -0700

    Provide needed data dependencies as Dagger modules

--- data/src/main/kotlin/com/addhen/livefront/data/di/DataModule.kt
+++ data/src/main/kotlin/com/addhen/livefront/data/di/DataModule.kt
@@ -3,10 +3,57 @@
 
 package com.addhen.livefront.data.di
 
+import com.addhen.livefront.data.api.GithubApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://api.github.com/"
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DataModule
+object DataModule {
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(logging: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(logging).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient, json: Json): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
+            json.asConverterFactory("application/json; charset=UTF8".toMediaType()),
+        ).client(client).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGithubApiService(retrofit: Retrofit): GithubApiService {
+        return retrofit.create(GithubApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json {
+        return Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+        }
+    }
+}
