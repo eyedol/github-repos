ec171da0d53e990011c7f4da962c189d30994473
Henry Addo <henry@addhen.org>
2025-05-14 21:43:51 -0700

    Remove unused imports in App file

--- app/src/main/kotlin/com/addhen/livefront/App.kt
+++ app/src/main/kotlin/com/addhen/livefront/App.kt
@@ -4,14 +4,9 @@
 package com.addhen.livefront
 
 import android.app.Application
-import android.content.Context
 import androidx.multidex.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
 import timber.log.Timber
-import javax.inject.Inject
 
 @HiltAndroidApp
 class App : Application() {
