97a7b11cd0c3a2abf49868be1437dd82fa7750db
Henry Addo <henry@addhen.org>
2025-05-14 21:43:13 -0700

    Rename and simplify NetworkConnectivityStatus component
    Renamed the file to fix a typo in the name and removed unused
    imports and unnecessary state management logic.

--- app/src/main/kotlin/com/addhen/livefront/ui/component/NetworkConnectivityStatus.kt
+++ app/src/main/kotlin/com/addhen/livefront/ui/component/NetworkConnectivityStatus.kt
@@ -19,29 +19,23 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.addhen.livefront.ConnectionState
 import com.addhen.livefront.R
-import com.addhen.livefront.currentConnectivityState
-import com.addhen.livefront.observeConnectivityAsFlow
 import com.addhen.livefront.ui.theme.errorLight
 import com.addhen.livefront.ui.theme.primaryLight
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.distinctUntilChanged
 
 @ExperimentalAnimationApi
 @ExperimentalCoroutinesApi
